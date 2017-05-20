dcac6789b725b9886d064442001708d80b57484d
Shaojung <shaojung@gmail.com>
2017-05-20 15:55:58 +0800

    turn on wifi

--- app/src/main/java/com/example/teacher/db052002/MainActivity.java
+++ app/src/main/java/com/example/teacher/db052002/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.teacher.db052002;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     WifiManager wifi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
+    public void clickScan(View v) {
+        wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (wifi.isWifiEnabled() == false) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setTitle("Remind");
+            dialog.setMessage("Your Wi-Fi is not enabled, enable?");
+            dialog.setIcon(android.R.drawable.ic_dialog_info);
+            dialog.setCancelable(false);
+            dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // TODO Auto-generated method stub
+                    wifi.setWifiEnabled(true);
+                    Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled", Toast.LENGTH_LONG).show();
+                }
+            });
+            dialog.show();
+        }
+    }
+
 }
